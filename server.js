f6efde886f18ac66f4083edac23c26f3f4d6501f
N7 GIL <gilbertoaescobedo@gmail.com>
2022-09-09 20:15:03 -0700

    created main handlebars, stylesheet, server.js, and .env files

---
+++ server.js
@@ -0,0 +1,9 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
