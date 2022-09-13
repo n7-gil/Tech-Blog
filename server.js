7150c7ff9f8670b82f9feca8b3789c66a7a4c1cf
N7 GIL <gilbertoaescobedo@gmail.com>
2022-09-12 21:57:22 -0700

    added config files, controller files and server.js files

--- server.js
+++ server.js
@@ -3,7 +3,48 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
+const helpers = require("./utils/helper");
+const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Creating Session
+const sess = {
+  secret: "victoria secret",
+
+  // cookie
+  cookie: {
+    maxAge: 1800000, // 30 Mins
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+
+  //  Session store
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Now listening on http://localhost:${PORT} 🚀`);
+  });
+});
